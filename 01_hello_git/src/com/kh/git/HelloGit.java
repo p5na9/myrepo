fdceb4baea412835dcef88be0f8104f67c521e2d
p5na9 <sp5na92@gmail.com>
2021-10-25 17:19:36 +0900

    hello web 출력문~

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
 		// TODO Auto-generated method stub
 		System.out.println("hello world~");
 		System.out.println("hello git!");
+		System.out.println("hello web.");
 	}
 
 }
