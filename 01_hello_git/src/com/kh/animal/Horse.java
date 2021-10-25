e8d7aa8278c1b861a14b706f3811df12d8c140ef
p5na9 <sp5na92@gmail.com>
2021-10-25 18:10:19 +0900

    horse

--- 01_hello_git/src/com/kh/animal/Horse.java
+++ 01_hello_git/src/com/kh/animal/Horse.java
@@ -1,10 +1,7 @@
 package com.kh.animal;
 
 public class Horse {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void run() {
+		System.out.println();
 	}
-
 }
