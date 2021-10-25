f83973291f8541261655c65f03f23e837dda6cca
p5na9 <sp5na92@gmail.com>
2021-10-25 20:08:41 +0900

    2 3333333 4

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -10,9 +10,8 @@ public class HelloGit {
 		System.out.println("hello animal~");
 	}
 	public void merge() {
-		System.out.println(1);
 		System.out.println(2);
-		System.out.println(3);
+		System.out.println(3333333);
 		System.out.println(4);
 	}
 }
